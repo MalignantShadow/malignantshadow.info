c918cadcb6203455971fe359063d3c7ce076af59
Shad0w <malignant.shadowborne@gmail.com>
2019-02-03 23:23:43 -0800

    eject and setup asc/www split

---
+++ src/index.www.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import * as serviceWorker from './serviceWorker';
+
+const App = () => (
+  <div>Hi there!</div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//serviceWorker.unregister();
