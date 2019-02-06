0fe1f27e86404002bdfa33d97aeca27bc60b67d1
Shad0w <malignant.shadowborne@gmail.com>
2019-02-06 03:23:16 -0800

    Add a RepoButton component

--- src/components/common/links.js
+++ src/components/common/links.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { withStyles } from '@material-ui/core/styles'
 import Tooltip from '@material-ui/core/Tooltip'
 import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+
 import GhIcon from './icons/GitHub'
+import BbIcon from './icons/BitBucket'
 
 export const IconLink = ({children, href, newTab, tooltip, ...other}) => (
   <Tooltip title={tooltip}>
@@ -34,4 +38,35 @@ GitHubLink.propTypes = {
 
 GitHubLink.defaultProps = {
   icon: GhIcon
+}
+
+const rbStyles = theme => ({
+  icon: { marginRight: theme.spacing.unit }
+})
+
+export const RepoButton = withStyles(rbStyles)(({classes, type, repo, ...other}) => {
+
+  let Icon, href, host
+  if(type === "github") {
+    Icon = GhIcon
+    href = `https://www.github.com/${repo}`
+    host = "GitHub"
+  } else if(type === "bitbucket") {
+    Icon = BbIcon
+    href = `https://www.bitbucket.com/${repo}`
+    host = "BitBucket"
+  }
+
+  return (
+    <Button href={href} {...other} component="a" target="_blank" rel="noopener noreferrer">
+      <Icon className={classes.icon}/>
+      View on {host || type}
+    </Button>
+  )
+})
+
+RepoButton.propTypes = {
+  type: PropTypes.string.isRequired,
+  repo: PropTypes.string.isRequired,
+  href: PropTypes.string
 }
