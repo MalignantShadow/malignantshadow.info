3eb6bdb12cb7a50871ed605755f8a801bbb2fb4b
Shad0w <malignant.shadowborne@gmail.com>
2019-02-10 20:51:07 -0800

    Add a page layout component for ascref

---
+++ src/components/asc/AscPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+
+import SiteBreadcrumbs from '../common/app/SiteBreadcrumbs'
+import Asc from './icons/Asc'
+import routing from '../../lib/asc/routeInfo'
+
+export default withStyles(theme => ({
+  wrapper: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  stretch: {
+    flexGrow: 1
+  },
+  content: {
+    marginTop: theme.spacing.unit
+  }
+}))(({classes, children, rightContent}) => (
+  <React.Fragment>
+    <div className={classes.wrapper}>
+      <SiteBreadcrumbs icon={Asc}>{routing}</SiteBreadcrumbs>
+      <div className={classes.stretch}/>
+      <div className={classes.rightContent}>{rightContent}</div>
+    </div>
+    <div className={classes.content}>
+      {children}
+    </div>
+  </React.Fragment>
+))
