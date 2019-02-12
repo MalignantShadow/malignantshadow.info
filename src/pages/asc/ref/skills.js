f27c17b3c2a540b54e685f78103427b838abe15a
Shad0w <malignant.shadowborne@gmail.com>
2019-02-12 02:39:40 -0800

    Add barebones skills page

---
+++ src/pages/asc/ref/skills.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+
+import { AscPage } from '../../../components/asc'
+
+const styles = theme => ({
+
+})
+
+class Skills extends React.Component {
+
+  render() {
+    return (
+      <AscPage>
+
+      </AscPage>
+    )
+  }
+
+}
+
+export default withStyles(styles)(Skills)
