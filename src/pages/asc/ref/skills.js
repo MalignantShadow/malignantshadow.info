f793e6a346db367751d88673705b21703eda0b94
Shad0w <malignant.shadowborne@gmail.com>
2019-02-15 17:02:59 -0800

    Add some content to skills page

--- src/pages/asc/ref/skills.js
+++ src/pages/asc/ref/skills.js
@@ -1,23 +1,46 @@
 import React from 'react'
 
 import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
 
-import { AscPage } from '../../../components/asc'
+import { AscPage, AscSection } from '../../../components/asc'
 
-const styles = theme => ({
+const Section = withStyles(theme => ({
+  heading: { borderColor: theme.asc.abilities.main }
+}))(({classes, children, ...other}) => (
+  <AscSection className={classes.heading} {...other}>{children}</AscSection>
+))
 
-})
+export default withStyles(theme => ({
 
-class Skills extends React.Component {
-
-  render() {
-    return (
-      <AscPage>
-
-      </AscPage>
-    )
-  }
-
-}
-
-export default withStyles(styles)(Skills)
+}))(({classes}) => (
+  <AscPage>
+    <Section variant="title" title="Skills" subtitle="reference">
+      <Typography paragraph>
+        A skill is a pre-defined type of action in the game, associated with one of the six aptitudes (with the exception
+        of Constitution). This section details the appropriate time to use each of these skills. Note that the GM may
+        decide, based on the situation, to you roll a general aptitude check instead of a skill check.
+      </Typography>
+    </Section>
+    <Section id="#cha" variant="h1" title="Charisma" caption="Measures one's strength of character.">
+      <Typography  paragraph>
+        The GM calls for Charisma skill checks when your character wants to talk or act/perform their way out of (or into)
+        a situation.
+      </Typography>
+    </Section>
+    <Section id="#deceoption" variant="h2" title="Deception">
+      <Typography paragraph>
+        A Deception check determines how well your character can hide the truth. Deception differs from persuasion; while
+        your character may be trying to convince or "persuade" another character, it is considered <b>deception</b> if
+        your character is hiding the truth in some way. Most of the time, this is done with vague wording or telling a
+        direct or obvious lie. Common situations include <b>impersonation</b>, <b>giving someone false assurances</b>
+        , and <b>passing misleading information</b>.
+      </Typography>
+    </Section>
+    <Section id="intimidation" variant="h2" title="Intimidation">
+      <Typography paragraph>
+        
+      </Typography>
+    </Section>
+  </AscPage>
+))
