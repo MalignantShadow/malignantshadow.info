8d77ad85aae92210df74080e898d8e3bc016e527
Shad0w <malignant.shadowborne@gmail.com>
2019-02-15 17:01:18 -0800

    Update AscPage to not use a TOC

--- src/components/asc/AscPage.js
+++ src/components/asc/AscPage.js
@@ -1,27 +1,10 @@
 import React from 'react'
-import classNames from 'classnames'
 
 import { withStyles } from '@material-ui/core/styles'
 
 import { SiteBreadcrumbs } from '../common/app'
-import TocFrame from './TocFrame'
-import TocItem from './TocItem'
 import Asc from './icons/Asc'
 import routing from '../../lib/asc/routeInfo'
-import { slug } from "../../lib/routing"
-
-const getTocItems = (toc, classes, active, depth = 0) => {
-  let tocItems = []
-  toc.forEach((e) => {
-    const hash = e.hash || slug(e.title)
-    tocItems.push(<TocItem key={hash} className={classNames(classes.item, {
-      [classes.active]: e.hash === active
-    })} depth={depth} href={`#${hash}`}>{e.title}</TocItem>)
-    if(e.children)
-      tocItems = tocItems.concat(getTocItems(e.children, classes, active, depth + 1))
-  })
-  return tocItems
-}
 
 export default withStyles(theme => ({
   wrapper: {
@@ -36,25 +19,14 @@ export default withStyles(theme => ({
   },
   toc: {
     width: 225
-  },
-  tocItem: {
-    color: theme.palette.text.secondary
-  },
-  tocActive: {
-    color: theme.palette.text.primary
   }
-}))(({classes, children, rightContent, toc, tocActive}) => (
+}))(({classes, children, rightContent}) => (
   <div className={classes.root}>
     <div className={classes.wrapper}>
       <SiteBreadcrumbs icon={Asc}>{routing}</SiteBreadcrumbs>
       <div className={classes.stretch}/>
       <div className={classes.rightContent}>{rightContent}</div>
     </div>
-    {toc &&
-      <TocFrame className={classes.toc}>
-        {getTocItems(toc, {item: classes.tocItem, active: classes.tocActive}, tocActive)}
-      </TocFrame>
-    }
     <div className={classes.content}>
       {children}
     </div>
