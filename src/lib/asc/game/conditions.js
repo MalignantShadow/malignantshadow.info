a71d1d249b0401718a1d6d885cb529c9d46228f3
Shad0w <malignant.shadowborne@gmail.com>
2019-02-16 21:51:06 -0800

    Edit wording to convey a cost instead of a predicate

--- src/lib/asc/game/conditions.js
+++ src/lib/asc/game/conditions.js
@@ -1,7 +1,7 @@
 const c = (name, desc) => ({ name, desc })
 
 export const prone = c("Prone",  [
-  "The unit's only movement option is to crawl or stand up. Standing up requires half movement and ends this condition.",
+  "The unit's only movement option is to crawl or stand up. Standing up costs half the unit's movement and ends this condition.",
   "The unit has disadvantage on attack rolls",
   "Attack rolls against the unit have disadvantage. If the attacker is within 5 feet, the attack roll has advantage instead."
 ])
