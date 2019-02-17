1b07e4b944e2560ddaa5c2d5a657fe7b1c9fc062
Shad0w <malignant.shadowborne@gmail.com>
2019-02-17 04:53:24 -0800

    Set TransitionComponent to undefined to force the default transition

--- src/components/asc/terms/GameTerm.js
+++ src/components/asc/terms/GameTerm.js
@@ -6,6 +6,15 @@ import { dictionary } from '../../../lib/asc/game'
 export default ({term: termProp}) => {
   const term = dictionary[termProp]
   return (
-    <TermBase text={term.name} TooltipProps={{title: term.desc, classes: {}, placement: "top"}} href={term.href}/>
+    <TermBase
+      text={term.name}
+      TooltipProps={{
+        title: term.desc,
+        classes: {},
+        placement: "top",
+        TransitionComponent: undefined
+      }} 
+      href={term.href}
+    />
   )
 }
