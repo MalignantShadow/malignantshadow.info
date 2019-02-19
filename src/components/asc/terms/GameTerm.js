8c5661f574de4527d161edb8262c0c4715426df2
Shad0w <malignant.shadowborne@gmail.com>
2019-02-19 04:42:15 -0800

    Support plural game terms

--- src/components/asc/terms/GameTerm.js
+++ src/components/asc/terms/GameTerm.js
@@ -3,11 +3,11 @@ import React from 'react'
 import TermBase from './TermBase'
 import { dictionary } from '../../../lib/asc/game'
 
-export default ({term: termProp, ...other}) => {
+export default ({term: termProp, plural, ...other}) => {
   const term = dictionary[termProp]
   return (
     <TermBase
-      text={term.name}
+      text={plural ? term.plural || `${term.name}s` : term.name}
       TooltipProps={{
         title: term.desc,
         classes: {},
