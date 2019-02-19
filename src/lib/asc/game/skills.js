d2c5dd1dce1e7839a98943456b0b1e2439ff1aea
Shad0w <malignant.shadowborne@gmail.com>
2019-02-19 06:01:43 -0800

    Split skill descriptions into seperate file

--- src/lib/asc/game/skills.js
+++ src/lib/asc/game/skills.js
@@ -1,105 +1,246 @@
-const s = (name, desc, aptitude, examples) => ({name, desc, aptitude, examples})
+import React from 'react'
 
-export const athletics = s("Athletics", "Pertains to situations that require physical strength and endurance.", "Strength", "Running, climbing, long jumping")
+import Typography from '@material-ui/core/Typography'
 
-const skills = [{
-  name: "Athletics",
-  desc: "Pertains to situations that require physical strength and endurance.",
-  aptitude: "Strength",
-  examples: "Running, climbing, long jumping"
-}, {
-  name: "Acrobatics",
-  desc: "Pertains to situations that require balance or precision.",
-  aptitude: "Dexterity",
-  examples: "Stunts (flips, dives, rolls), walking across a slippery surface.",
-}, {
-  name: "Animal Handling",
-  desc: "Pertains to situations involving animals.",
-  aptitude: "Wisdom",
-  examples: "Taming an animal, training a pet, warding off a predator."
-}, {
-  name: "Deception",
-  desc: "Pertains to deception of an adversary through trickery or wordplay.",
-  aptitude: "Charisma",
-  examples: "Lying (by omission or blatancy), deceiving word usage."
-}, {
-  name: "Driving",
-  desc: "Pertains to driving a vehicle and performing a potentially dangerous or advanced maneuver.",
-  aptitude: "Dexterity",
-  examples: "Drifting around a corner, performing a successful PIT maneuver, performing a stunt."
-}, {
-  name: "Engineering",
-  desc: "Pertains to attempting to fix or alter a piece of equipment or hardware.",
-  aptitude: "Intelligence",
-  examples: "Repairing a broken down vehicle, modifying or improvising a weapon",
-  tool: "Engineering Tools"
-}, {
-  name: "Hacking",
-  desc: "Pertains to attempting to alter, block, or intercept electronic signals.",
-  aptitude: "Intelligence",
-  examples: "Hacking an electronically locked door, altering an electronic display",
-  tool: "Hacking Tools"
-}, {
-  name: "History",
-  desc: "Pertains to recalling information about the history of Ecumene.",
-  aptitude: "Intelligence",
-  examples: "Recalling the name of a war hero, recalling historical information about a city, recalling a historical battle."
-}, {
-  name: "Insight",
-  desc: "Pertains to the analyzation of a character or creature's voice or body language to gain insight on their thoughts and feelings.",
-  aptitude: "Wisdom",
-  examples: "Trying to deduce if a character is lying, perceiving hoe a character feels about a piece information that was just shared."
-}, {
-  name: "Intimidation",
-  desc: "Pertains to the intimidation or scaring of other characters.",
-  aptitude: "Charisma",
-  examples: "Announcing an ultimatum, yelling, showing a large display of aggression"
-}, {
-  name: "Investigation",
-  desc: "Pertains to searching areas through the use of active searching for specific information.",
-  aptitude: "Intelligence",
-  examples: "Searching a city for a good restaurant, checking for traps, asking townspeople for the location of another character."
-}, {
-  name: "Medicine",
-  desc: "Pertains to situations requiring medical knowledge.",
-  aptitude: "Wisdom",
-  examples: "Applying first aid, identifying a drug"
-}, {
-  name: "Nature",
-  desc: "Pertains to the identification and information of fauna and flora in Ecumene.",
-  aptitude: "Intelligence",
-  examples: "Identifying animal tracks, identifying a plant, deducing whether a plant poisonous."
-}, {
-  name: "Perception",
-  desc: "Pertains to the perception of information pertaining to the senses of touch, smell, sight, hearing, and taste.",
-  aptitude: "Wisdom",
-  examples: "Watching a crowd for anyone suspicious, eavesdropping on a conversation."
-}, {
-  name: "Performance",
-  desc: "Pertains to the performance of a character when they play an instrument, create an art piece, or another action considered a 'performance.'",
-  aptitude: "Charisma",
-  examples: "Playing a role in a play, playing an instrument, dancing, painting"
-}, {
-  name: "Persuasion",
-  desc: "Pertains to the persuasion of a character to be sympathetic towards another character's view or cause, typically by providing a benefit.",
-  aptitude: "Charisma",
-  examples: "Convincing an enemy to lay down their arms, "
-}, {
-  name: "Sleight of Hand",
-  desc: "Pertains to the quick and precise movements of a character's hands and fingers to accomplish a task.",
-  aptitude: "Dexterity",
-  examples: "Pickpocketing, cheating in a card game, juggling"
-}, {
-  name: "Stealth",
-  desc: "Pertains to movement and actions done in such a way as to not rise suspicion.",
-  aptitude: "Dexterity",
-  examples: "Hiding, sneaking, moving without being seen"
-}, {
-  name: "Survival",
-  desc: "Wisdom",
-  aptitude: "Wisdom",
-  examples: "Tracking a group using tracks that may not be clearly visible, recalling information about improvised shelter."
-}]
+import {
+  ClassificationTerm,
+  ConditionTerm,
+  GameTerm
+} from '../../../components/asc'
+
+const s = (name, aptitude, caption, desc) => ({name, aptitude, caption, desc})
+
+export const acrobatics = s(
+  "Acrobatics",
+  "Dexterity",
+  "",
+  () => (
+    <Typography paragraph>
+      An Acrobatics check determines how well you can keep your balance or footing during a task. Common situations typically encompass
+      slippery or unstable areas; such as walking across ice or a narrow walkway/bridge. The <GameTerm term="gm"/> may also ask for this check when
+      you want to make a precise adjustment while running, like jumping over a table. A critical fail on an Acrobatics check may cause your
+      character to become <ConditionTerm term="prone"/>.
+    </Typography>
+  )
+)
+
+export const animalHandling = s(
+  "Animal Handling",
+  "Wisdom",
+  "",
+  () => (
+    <Typography paragraph>
+      An Animal Handling check determines how well you can control or coerce an animal. Common instances of this skill check include <b>training a
+      pet</b>, <b>convicing an animal you are friendly</b>, and <b>determing an animal's intentions</b>.
+    </Typography>
+  )
+)
+
+export const athletics = s(
+  "Athletics",
+  "Strength",
+  "May be contested by: Athletics or Acrobatics",
+  () => (
+    <Typography paragraph>
+      An Athletics check determines how well you can endure a troubling situation or apply brute force to an object. Common examples
+      include <b>treading water for an extended period of time</b>, <b>forcing open a door</b>, <b>moving a heavy object</b>, and <b>standing
+      you ground while something tries to move you</b>.
+    </Typography>
+  )
+)
+
+export const deception = s(
+  "Deception",
+  "Charisma",
+  "Contested with: Insight",
+  () => (
+    <Typography paragraph>
+      A Deception check determines how well you can hide the truth. Deception differs from Persuasion - while
+      you may be trying to convince or "persuade" another character, it is considered <b>deception</b> if
+      you are hiding the truth in some way. Most of the time, this is done with vague wording or telling a
+      direct or obvious lie. Common situations include <b>impersonation</b>, <b>giving someone false assurances</b>
+      , and <b>passing misleading information</b>.
+    </Typography>
+  )
+)
+
+export const history = s(
+  "History",
+  "Intelligence",
+  "",
+  () => (
+    <Typography paragraph>
+      A History check determines how well and accurate you can recall information about the history of Ecumene. For instance, his check may be
+      prompted when you try to <b>indentify a building's historical significance</b>, <b>recall a historical battle</b>, or <b>identify a
+      legendary person from a picture of them</b>.
+    </Typography>
+  )
+)
+
+export const insight = s(
+  "Insight",
+  "Wisdom",
+  "Contested by: Deception or Persuasion",
+  () => (
+    <Typography paragraph>
+      An Insight check determines how well you can deduce a person's intentions based on their body language, speech patterns, and mannerisms.
+      Insight checks typically have a lower <GameTerm term="dc"/> the better you know a person.
+    </Typography>
+  )
+)
+
+export const intimidation = s(
+  "Intimidation",
+  "Charisma",
+  "",
+  () => (
+    <Typography paragraph>
+      An Intimidation check determines how well you can threaten or scare another person or creature. A threat can be
+      verbal, by using a specific combination of words to create an elaborate threat, or they can be physical, such as harming
+      the person and stating that will continue to do so unless they unveil the information you want. Verbal threats can be implied or explicit.
+    </Typography>
+  )
+)
+
+export const investigation = s(
+  "Investigation",
+  "Intelligence",
+  "",
+  () => (
+    <React.Fragment>
+      <Typography paragraph>
+        An Investigation check determines how well you can make a deduction or extrapolate information. Typical examples of Investigation checks
+        include <b>asking around for the location of a specific landmark</b>, <b>searching for traps</b>, or <b>determining the weak points
+        in a structure</b>.
+      </Typography>
+      <Typography paragraph>
+        Investigation differs from Perception. Perception is usually considered to be passive (noticing something <b>as it
+        happens</b>), while Investigation is usually prompted when a character makes a conscious decision to search for a specific thing.
+        Investigation also implies <i>active</i> searching over time by moving objects or asking multiple people. The general rule of thumb is
+        "find means Investigation; see/notice means Perception."
+      </Typography>
+    </React.Fragment>
+  )
+)
+
+export const medicine = s(
+  "Medicine",
+  "Wisdom",
+  "",
+  () => (
+    <React.Fragment>
+      <Typography paragraph>
+        A Medicine check determines how well you can use your medical knowledge and experience in a particular situation. The most common examples
+        are <b>diagnosing a disease or infection</b> and <b>applying first aid</b>.
+      </Typography>
+      <Typography paragraph>
+        The <GameTerm term="gm"/> may require you to be proficient in this skill in order to pass certain Medicine checks.
+      </Typography>
+    </React.Fragment>
+  )
+)
+
+export const nature = s(
+  "Nature",
+  "Intelligence",
+  "",
+  () => (
+    <Typography paragraph>
+      A Nature checks how well you can recall information about the landscapes, plants, animals, and weather cycles of Ecumene.
+    </Typography>
+  )
+)
+
+export const perception = s(
+  "Perception",
+  "Wisdom",
+  "",
+  () => (
+    <React.Fragment>
+      <Typography paragraph>
+        A Perception check determines how well you sense the presence of an object or creature using your senses. Common examples
+        include <b>listening to a conversation through a wall/door</b>, <b>spotting a specific face in a crowd of people</b>,
+        or <b>discerning the identity of an obscured object/creature</b>.
+      </Typography>
+      <Typography paragraph>
+        Perception differs from Investigation. Perception is normally used to notice an event as it is happening, while Investigation implies an
+        active search. The general rule of thumb is "see/notice means Perception; find means Investigation."
+      </Typography>
+    </React.Fragment>
+  )
+)
+
+export const performance = s(
+  "Performance",
+  "Charisma",
+  "",
+  () => (
+    <Typography paragraph>
+      A Performance check indicates how well you can dance, play an instrument, act, or perform some other type of entertainment.
+      Available entertainment and standards vary by area.
+    </Typography>
+  )
+)
+
+export const persuasion = s(
+  "Persuasion",
+  "Charisma",
+  "Contested with: Insight",
+  () => (
+    <Typography paragraph>
+      A Persuasion check determines how well you can influence another person or creature's actions with your own benevolent actions,
+      without false pretenses. Examples include <b>convincing a group you are friendly by laying down your weapons</b>, <b>bribing a
+      gaurd</b>, or <b>convincing a group to agree with your point of view</b>.
+    </Typography>
+  )
+)
+
+export const sleightOfHand = s(
+  "Sleight of Hand",
+  "Dexterity",
+  "Contested with: Perception",
+  () => (
+    <Typography paragraph>
+      A Sleight of Hand check determines how well you can conceal or move an object without anyone noticing. Common examples
+      include <b>pickpocketing</b>, <b>placing a small object into somone else's pocket</b>, and <b>cheating in a card game</b>.
+    </Typography>
+  )
+)
+
+export const stealth = s(
+  "Stealth",
+  "Dexterity",
+  "Contested with: Perception",
+  () => (
+    <React.Fragment>
+      <Typography paragraph>
+        A Stealth check detmines how well you can stay out of sight from onlookers. Typical situations include <b>eavesdropping</b> and <b>traveling
+        through a crowded/gaurded area</b> <i>without being detected</i>.
+      </Typography>
+      <Typography paragraph>
+        <ClassificationTerm term="shadowborne"/> are particularly adept at Stealth.
+      </Typography>
+    </React.Fragment>
+  )
+)
+
+const skills = [
+  acrobatics,
+  animalHandling,
+  athletics,
+  deception,
+  history,
+  insight,
+  intimidation,
+  investigation,
+  medicine,
+  nature,
+  perception,
+  performance,
+  persuasion,
+  sleightOfHand,
+  stealth
+]
 
 export default skills
 export const byAptitude = skills.reduce((acc, obj) => {
--- src/pages/asc/ref/skills.js
+++ src/pages/asc/ref/skills.js
@@ -7,10 +7,10 @@ import Divider from '@material-ui/core/Divider'
 import {
   AscPage,
   AscSection,
-  ConditionTerm,
-  ClassificationTerm,
   GameTerm,
  } from '../../../components/asc'
+ import { skillsByAptitude } from '../../../lib/asc/game'
+ import { slug } from '../../../lib/routing'
  import { styledTocItem } from '../../../components/asc/util'
 
 const Section = withStyles(theme => ({
@@ -21,29 +21,29 @@ const Section = withStyles(theme => ({
 
 const SkillTocItem = styledTocItem(theme => theme.asc.skills)
 
+const getTocItems = (key) => skillsByAptitude[key].map((e, i) => (
+  <SkillTocItem key={key + i} href={`#${slug(e.name)}`} depth={1}>{e.name}</SkillTocItem>
+))
+
+const getSections = (key) => skillsByAptitude[key].map(({name, caption, desc: Desc}, i) => (
+  <Section key={name + i} variant="h2" title={name} caption={caption}>
+    <Desc/>
+  </Section>
+))
+
 const toc = <React.Fragment>
   <SkillTocItem title href="#topContent">Reference: Skills</SkillTocItem>
   <Divider/>
   <SkillTocItem href="#cha">Charisma</SkillTocItem>
-  <SkillTocItem href="#deception" depth={1}>Deception</SkillTocItem>
-  <SkillTocItem href="#intimidation" depth={1}>Intimidation</SkillTocItem>
-  <SkillTocItem href="#performance" depth={1}>Performance</SkillTocItem>
-  <SkillTocItem href="#persuasion" depth={1}>Persuasion</SkillTocItem>
+  {getTocItems("Charisma")}
   <SkillTocItem href="#dex">Dexterity</SkillTocItem>
-  <SkillTocItem href="#acrobatics" depth={1}>Acrobatics</SkillTocItem>
-  <SkillTocItem href="#sleight-of-hand" depth={1}>Sleight of Hand</SkillTocItem>
-  <SkillTocItem href="#stealth" depth={1}>Stealth</SkillTocItem>
+  {getTocItems("Dexterity")}
   <SkillTocItem href="#int">Intelligence</SkillTocItem>
-  <SkillTocItem href="#history" depth={1}>History</SkillTocItem>
-  <SkillTocItem href="#investigation" depth={1}>Investigation</SkillTocItem>
-  <SkillTocItem href="#nature" depth={1}>Nature</SkillTocItem>
+  {getTocItems("Intelligence")}
   <SkillTocItem href="#str">Strength</SkillTocItem>
-  <SkillTocItem href="#athletics" depth={1}>Athletics</SkillTocItem>
+  {getTocItems("Strength")}
   <SkillTocItem href="#wis">Wisdom</SkillTocItem>
-  <SkillTocItem href="#animal-handling" depth={1}>Animal Handling</SkillTocItem>
-  <SkillTocItem href="#insight" depth={1}>Insight</SkillTocItem>
-  <SkillTocItem href="#medicine" depth={1}>Medicine</SkillTocItem>
-  <SkillTocItem href="#perception" depth={1}>Perception</SkillTocItem>
+  {getTocItems("Wisdom")}
 </React.Fragment>
 
 export default () => (
@@ -68,151 +68,30 @@ export default () => (
         a situation.
       </Typography>
     </Section>
-    <Section variant="h2" title="Deception" caption="Contested with: Insight">
-      <Typography paragraph>
-        A Deception check determines how well you can hide the truth. Deception differs from Persuasion - while
-        you may be trying to convince or "persuade" another character, it is considered <b>deception</b> if
-        you are hiding the truth in some way. Most of the time, this is done with vague wording or telling a
-        direct or obvious lie. Common situations include <b>impersonation</b>, <b>giving someone false assurances</b>
-        , and <b>passing misleading information</b>.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Intimidation">
-      <Typography paragraph>
-        An Intimidation check determines how well you can threaten or scare another person or creature. A threat can be
-        verbal, by using a specific combination of words to create an elaborate threat, or they can be physical, such as harming
-        the person and stating that will continue to do so unless they unveil the information you want. Verbal threats can be implied or explicit.
-      </Typography>
-      <ul>
-        <li><Typography>
-          An <i>explicit</i> threat is a statement that can be taken as a threat at face value, such as "<b>I'm gonna gouge out your eyes with a
-          spoon</b>."
-        </Typography></li>
-        <li><Typography>
-          An <i>implicit</i> threat is one that requires a bit of thinking in order to be considered a threat, such as "<b>That's a nice
-         house, it'd be a shame if it caught fire</b>."
-        </Typography></li>
-      </ul>
-    </Section>
-    <Section variant="h2" title="Performance">
-      <Typography paragraph>
-        A Performance check indicates how well you can dance, play an instrument, act, or perform some other type of entertainment.
-        Available entertainment and standards vary by area.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Persuasion" caption="Contested with: Insight">
-      <Typography paragraph>
-        A Persuasion check determines how well you can influence another person or creature's actions with your own benevolent actions,
-        without false pretenses. Examples include <b>convincing a group you are friendly by laying down your weapons</b>, <b>bribing a
-        gaurd</b>, or <b>convincing a group to agree with your point of view</b>.
-      </Typography>
-    </Section>
+    {getSections("Charisma")}
     <Section id="dex" variant="h1" caption="Measures one's flexibility and agility." title="Dexterity">
       <Typography paragraph>
         The <GameTerm term="gm"/> may call for a Dexterity check when your character need to complete a task with balance, precision, or silence.
       </Typography>
     </Section>
-    <Section variant="h2" title="Acrobatics">
-      <Typography paragraph>
-        An Acrobatics check determines how well you can keep your balance or footing during a task. Common situations typically encompass
-        slippery or unstable areas; such as walking across ice or a narrow walkway/bridge. The <GameTerm term="gm"/> may also ask for this check when
-        you want to make a precise adjustment while running, like jumping over a table. A critical fail on an Acrobatics check may cause your
-        character to become <ConditionTerm term="prone"/>.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Sleight of Hand" caption="Contested with: Perception">
-      <Typography paragraph>
-        A Sleight of Hand check determines how well you can conceal or move an object without anyone noticing. Common examples
-        include <b>pickpocketing</b>, <b>placing a small object into somone else's pocket</b>, and <b>cheating in a card game</b>.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Stealth" caption="Contested with: Perception">
-      <Typography paragraph>
-        A Stealth check detmines how well you can stay out of sight from onlookers. Typical situations include <b>eavesdropping</b> and <b>traveling
-        through a crowded/gaurded area</b> <i>without being detected</i>.
-      </Typography>
-      <Typography paragraph>
-        <ClassificationTerm term="shadowborne"/> are particularly adept at Stealth.
-      </Typography>
-    </Section>
+    {getSections("Dexterity")}
     <Section id="int" variant="h1" title="Intelligence" caption="Measures one's mental acuity and memory.">
       <Typography paragraph>
           The <GameTerm term="gm"/> may call for an Intelligence check when to recall information or identify an object or place.
       </Typography>
     </Section>
-    <Section variant="h2" title="History">
-      <Typography paragraph>
-        A History check determines how well and accurate you can recall information about the history of Ecumene. For instance, his check may be
-        prompted when you try to <b>indentify a building's historical significance</b>, <b>recall a historical battle</b>, or <b>identify a
-        legendary person from a picture of them</b>.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Investigation">
-      <Typography paragraph>
-        An Investigation check determines how well you can make a deduction or extrapolate information. Typical examples of Investigation checks
-        include <b>asking around for the location of a specific landmark</b>, <b>searching for traps</b>, or <b>determining the weak points
-        in a structure</b>.
-      </Typography>
-      <Typography paragraph>
-        Investigation differs from Perception. Perception is usually considered to be passive (noticing something <b>as it
-        happens</b>), while Investigation is usually prompted when a character makes a conscious decision to search for a specific thing.
-        Investigation also implies <i>active</i> searching over time by moving objects or asking multiple people. The general rule of thumb is
-        "find means Investigation; see/notice means Perception."
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Nature">
-      <Typography paragraph>
-        A Nature checks how well you can recall information about the landscapes, plants, animals, and weather cycles of Ecumene.
-      </Typography>
-    </Section>
+    {getSections("Intelligence")}
     <Section variant="h1" title="Strength" caption="Measures one's ability to exert force.">
       <Typography paragraph>
         The <GameTerm term="gm"/> may call for a Strength when you want to use your raw physical power to accomplish a task.
       </Typography>
     </Section>
-    <Section variant="h2" title="Athletics" caption="May be contested by: Athletics or Acrobatics">
-      <Typography paragraph>
-        An Athletics check determines how well you can endure a troubling situation or apply brute force to an object. Common examples
-        include <b>treading water for an extended period of time</b>, <b>forcing open a door</b>, <b>moving a heavy object</b>, and <b>standing
-        you ground while something tries to move you</b>.
-      </Typography>
-    </Section>
+    {getSections("Strength")}
     <Section variant="h1" title="Wisdom" caption="Measures one's worldly experience">
       <Typography paragraph>
         The <GameTerm term="gm"/> may call for a Wisdom check when you want to use your social or environmental perceptiveness to solve a problem.
       </Typography>
     </Section>
-    <Section variant="h2" title="Animal Handling">
-      <Typography paragraph>
-        An Animal Handling check determines how well you can control or coerce an animal. Common instances of this skill check include <b>training a
-        pet</b>, <b>convicing an animal you are friendly</b>, and <b>determing an animal's intentions</b>.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Insight" caption="Contested by: Deception or Persuasion">
-      <Typography paragraph>
-        An Insight check determines how well you can deduce a person's intentions based on their body language, speech patterns, and mannerisms.
-        Insight checks typically have a lower <GameTerm term="dc"/> the better you know a person.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Medicine">
-      <Typography paragraph>
-        A Medicine check determines how well you can use your medical knowledge and experience in a particular situation. The most common examples
-        are <b>diagnosing a disease or infection</b> and <b>applying first aid</b>.
-      </Typography>
-      <Typography paragraph>
-        The <GameTerm term="gm"/> may require you to be proficient in this skill in order to pass certain Medicine checks.
-      </Typography>
-    </Section>
-    <Section variant="h2" title="Perception" caption="Contested by: Stealth">
-      <Typography paragraph>
-        A Perception check determines how well you sense the presence of an object or creature using your senses. Common examples
-        include <b>listening to a conversation through a wall/door</b>, <b>spotting a specific face in a crowd of people</b>,
-        or <b>discerning the identity of an obscured object/creature</b>.
-      </Typography>
-      <Typography paragraph>
-        Perception differs from Investigation. Perception is normally used to notice an event as it is happening, while Investigation implies an
-        active search. The general rule of thumb is "see/notice means Perception; find means Investigation."
-      </Typography>
-    </Section>
+    {getSections("Wisdom")}
   </AscPage>
 )
