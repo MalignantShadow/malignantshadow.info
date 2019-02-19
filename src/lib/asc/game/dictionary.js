f06db19e1870f0645d003685d318d5bbb39fdede
Shad0w <malignant.shadowborne@gmail.com>
2019-02-19 04:43:22 -0800

    Add DC term

--- src/lib/asc/game/dictionary.js
+++ src/lib/asc/game/dictionary.js
@@ -3,5 +3,6 @@ const t = (name, desc, other) => ({name, desc, ...other})
 export default {
   gm: t("GM", "Game Master"),
   advantage: t("Advantage", "Roll two d20s and choose the higher result"),
-  disadvantage: t("Disdvantage", "Roll two d20s and choose the lower result")
+  disadvantage: t("Disdvantage", "Roll two d20s and choose the lower result"),
+  dc: t("DC", "Difficulty Class")
 }
