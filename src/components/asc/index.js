ba3c3f4d2f936d3dd06a32a8422fe45ed6a6ac55
Shad0w <malignant.shadowborne@gmail.com>
2019-02-19 06:21:45 -0800

    Create a skill term component that uses the same information found on /ref/skills

--- src/components/asc/index.js
+++ src/components/asc/index.js
@@ -13,5 +13,6 @@ export {
   ClassificationTerm,
   DiceTerm,
   GameTerm,
+  SkillTerm,
   TermBase
  } from './terms'
---
+++ src/components/asc/terms/SkillTerm.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+import TermBase from './TermBase'
+import * as skills from '../../../lib/asc/game/skills'
+import { styleTerm, styleCategory } from './util'
+import { slug } from '../../../lib/routing'
+
+export default withStyles(theme => {
+  const color = theme.asc.skills
+  return {
+    ...styleTerm(color),
+    ...styleCategory(color, { color: "#000" }),
+    root: {
+      display: "flex",
+      flexDirection: "column"
+    },
+    statsWrapper: { //TODO: reused a bunch, make helper function
+      display: "flex",
+      paddingBottom: theme.spacing.unit,
+      marginBottom: theme.spacing.unit,
+      borderBottom: `3px solid ${color.main}`
+    },
+    stat: {
+      "&:not(:first-child)": {
+        marginLeft: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit,
+        borderLeft: `2px solid ${color.main}`
+      }
+    }
+  }
+})(({classes, term, ...other}) => {
+  const {name, aptitude, caption, desc: Desc} = skills[term]
+  return (
+    <TermBase
+      name={name}
+      category="Skill"
+      href={`/ref/skills#${slug(name)}`}
+      classes={{
+        root: classes.root,
+        term: classes.term,
+        category: classes.category
+      }}
+    >
+      <div className={classes.statsWrapper}>
+        <Typography className={classes.stat}><b>Aptitude</b>: {aptitude}</Typography>
+        { caption &&
+          <Typography className={classes.stat}><i>{caption}</i></Typography>
+        }
+      </div>
+      <Desc/>
+    </TermBase>
+  )
+})
