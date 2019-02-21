cdbbfaf5245513401304b2cafa085a93d66b7ced
Shad0w <malignant.shadowborne@gmail.com>
2019-02-21 01:36:27 -0800

    Remove indentation

--- src/components/asc/RpgTypography.js
+++ src/components/asc/RpgTypography.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import classNames from 'classnames'
 
 import {withStyles} from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
 export default withStyles(theme => ({
-  root: {
-    "&::first-letter": {
-      marginLeft: theme.spacing.unit * 6
-    }
-  }
+  title: {}
 }))(({classes, children, title, className, ...other}) => (
-  <Typography className={classNames(classes.root, className)} {...other}>
-    <b>{title}</b>. {children}
+  <Typography {...other}>
+    <b className={classes.title}>{title}{title && "."}</b> {children}
   </Typography>
 ))
