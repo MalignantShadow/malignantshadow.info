580fe44ab9dea6618745370d5d241257468857fa
Shad0w <malignant.shadowborne@gmail.com>
2019-02-22 05:46:44 -0800

    vscode formatting + increase top margin for each hero's name

--- src/pages/asc/ref/classificationPage.js
+++ src/pages/asc/ref/classificationPage.js
@@ -15,23 +15,23 @@ import {
   GameTerm,
   SkillTerm,
   TableOfContents
- } from '../../../components/asc'
- import { slug } from '../../../lib/routing'
- import { styledTocItem, styledSection } from '../../../components/asc/util'
- import * as classifications from '../../../lib/asc/game/classifications'
+} from '../../../components/asc'
+import { slug } from '../../../lib/routing'
+import { styledTocItem, styledSection } from '../../../components/asc/util'
+import * as classifications from '../../../lib/asc/game/classifications'
 
-const Features = ({children, idPrefix, Section}) => (
+const Features = ({ children, idPrefix, Section }) => (
   <React.Fragment>
-    {children.map(({title, desc: Desc, levels: [level], subFeatures}, i) => {
+    {children.map(({ title, desc: Desc, levels: [level], subFeatures }, i) => {
       const id = idPrefix && `${idPrefix}${slug(title)}`
       return (
         <React.Fragment key={i}>
           <Section id={id} variant="h2" title={title} caption={`Level ${level} Feature`}>
-            <Desc/>
+            <Desc />
           </Section>
-          {subFeatures && subFeatures.map(({title: subTitle, desc: SubDesc}, j) =>(
+          {subFeatures && subFeatures.map(({ title: subTitle, desc: SubDesc }, j) => (
             <Section id={`${id}.${slug(subTitle)}`} key={j} variant="h3" title={subTitle}>
-              <SubDesc/>
+              <SubDesc />
             </Section>
           ))}
         </React.Fragment>
@@ -61,15 +61,18 @@ export default withStyles(theme => ({
       fontSize: "1rem"
     }
   },
+  heroNameMargin: {
+    marginTop: theme.spacing.unit * 5
+  },
   toc: {
     "&:not(:first-child)": {
       marginTop: theme.spacing.unit * 3
     }
   }
-}))(withRouter(({classes, match: {params: {id}}}) => {
+}))(withRouter(({ classes, match: { params: { id } } }) => {
   const c = classifications[id]
-  if(!c) return "Encountered an uh-oh"
-  const {name, speed = 35, auraMod, icon: Icon, desc: Desc, traits, intrinsics, features, featureTableExtras, heroes} = c
+  if (!c) return "Encountered an uh-oh"
+  const { name, speed = 35, auraMod, icon: Icon, desc: Desc, traits, intrinsics, features, featureTableExtras, heroes } = c
 
   const colorSelector = theme => theme.asc.class[id]
   const Section = styledSection(colorSelector)
@@ -80,7 +83,7 @@ export default withStyles(theme => ({
   const toc = <React.Fragment>
     <TableOfContents className={classes.toc}>
       <TocItem title href="#topContent">Classification: {name}</TocItem>
-      <Divider/>
+      <Divider />
       <TocItem href="#traits">Traits</TocItem>
       <TocItem href="#hit-dice" depth={1}>Hit Dice</TocItem>
       <TocItem href="#proficiencies" depth={1}>Proficiencies</TocItem>
@@ -88,8 +91,8 @@ export default withStyles(theme => ({
     </TableOfContents>
     <TableOfContents className={classes.toc}>
       <TocItem title href="#heroes">Hero Studies</TocItem>
-      <Divider/>
-      {heroes && heroes.map(({name}, i) => (
+      <Divider />
+      {heroes && heroes.map(({ name }, i) => (
         <TocItem key={"hero" + i} href={`#hero.${slug(name)}`}>{name}</TocItem>
       ))}
     </TableOfContents>
@@ -99,7 +102,7 @@ export default withStyles(theme => ({
   features.forEach((e) => {
     e.levels.forEach((level, i) => {
       const index = level - 1
-      if(!featureNames[index]) featureNames[index] = []
+      if (!featureNames[index]) featureNames[index] = []
       featureNames[index].push(i > 0 ? e.titleAfterFirst || e.title : e.title)
     })
   })
@@ -112,49 +115,49 @@ export default withStyles(theme => ({
     ["Aura Modifier", auraMod],
     ["Speed", ` ${speed} feet`],
     [traits.resistance[0] || `${name} Resillience`, <React.Fragment>
-      You are <GameTerm term="resistance" variant="adj" args={[traits.resistance[1]]}/>
+      You are <GameTerm term="resistance" variant="adj" args={[traits.resistance[1]]} />
     </React.Fragment>],
     [traits.vulnerability[0] || `${name} Vulnerabilities`, <React.Fragment>
-      You are <GameTerm term="vulnerability" variant="adj" args={[traits.vulnerability[1]]}/>
+      You are <GameTerm term="vulnerability" variant="adj" args={[traits.vulnerability[1]]} />
     </React.Fragment>],
   ]
-  if(traits.extra)
+  if (traits.extra)
     sortedTraits = sortedTraits.concat(traits.extra)
 
   sortedTraits.sort(([a], [b]) => a - b)
 
-  const Table = withStyles(theme  => ({
+  const Table = withStyles(theme => ({
     headCell: {
       backgroundColor: theme.asc.class[id].main,
       color: theme.palette.getContrastText(theme.asc.class[id].main)
     },
     row: {
       "&:nth-child(4n+5) td:first-child": {
-          backgroundColor: theme.asc.class[id].main,
-          color: theme.palette.getContrastText(theme.asc.class[id].main)
+        backgroundColor: theme.asc.class[id].main,
+        color: theme.palette.getContrastText(theme.asc.class[id].main)
       }
     }
-  }))(({classes, features, ...other}) => (
+  }))(({ classes, features, ...other }) => (
     <AscTable classes={{
       headCell: classes.headCell,
-      row: classNames({[classes.row]: features})
-    }} {...other}/>
+      row: classNames({ [classes.row]: features })
+    }} {...other} />
   ))
 
 
   return (
-    <AscPage toc={toc} BreadcrumbProps={{extra: [{title: c.name}]}}>
+    <AscPage toc={toc} BreadcrumbProps={{ extra: [{ title: c.name }] }}>
       <Section
         id="topContent"
         variant="title"
         title={<React.Fragment>
-          <Icon className={classes.icon}/>
+          <Icon className={classes.icon} />
           {name}
         </React.Fragment>}
         subtitle="Classification Details"
-        classes={{heading: classes.pageTitle}}
+        classes={{ heading: classes.pageTitle }}
       >
-        <Desc/>
+        <Desc />
       </Section>
       <Section variant="h1" title="Traits">
         <Paper className={classes.tablePaper}>
@@ -166,28 +169,28 @@ export default withStyles(theme => ({
           />
         </Paper>
       </Section>
-      <Section variant="h2" title="Hit Dice"/>
+      <Section variant="h2" title="Hit Dice" />
       <Paper className={classes.tablePaper}>
-        <Table head={[{text: "Hit Dice", style: {width: 150}}, "HP at Level 1", "HP After Level 1"]} body={[[
-          <DiceTerm dice={intrinsics.hitDice}/>,
+        <Table head={[{ text: "Hit Dice", style: { width: 150 } }, "HP at Level 1", "HP After Level 1"]} body={[[
+          <DiceTerm dice={intrinsics.hitDice} />,
           <Calc>{intrinsics.hitDice.max} + your Constitution modifier</Calc>,
-          <Calc><DiceTerm dice={intrinsics.hitDice}/> (or {intrinsics.hitDice.avg + 1}) + your Constitution modifier per Level after 1</Calc>
-        ]]}/>
+          <Calc><DiceTerm dice={intrinsics.hitDice} /> (or {intrinsics.hitDice.avg + 1}) + your Constitution modifier per Level after 1</Calc>
+        ]]} />
       </Paper>
       <Section variant="h2" title="Proficiencies">
         <Typography>
-            <b>Weapons</b>: {intrinsics.prof.weapons.join(", ")}
+          <b>Weapons</b>: {intrinsics.prof.weapons.join(", ")}
         </Typography>
         <Typography>
           <b>Saving Throws</b>: {intrinsics.prof.savingThrows.join(", ")}
         </Typography>
         <Typography paragraph>
           <b>Skills</b>: Choose 3 from {intrinsics.prof.skills.map((e, i) => (
-            <React.Fragment key={i}><SkillTerm term={e}/>{i < intrinsics.prof.skills.length - 1 ? ", " : ""}</React.Fragment>
+            <React.Fragment key={i}><SkillTerm term={e} />{i < intrinsics.prof.skills.length - 1 ? ", " : ""}</React.Fragment>
           ))}
         </Typography>
       </Section>
-      <Section variant="h1" title="Features"/>
+      <Section variant="h1" title="Features" />
       <Paper className={classes.tablePaper}>
         <Table
           features
@@ -195,7 +198,7 @@ export default withStyles(theme => ({
             text: "Level",
             align: "center"
           },
-          ...(!featureTableExtras ? [] : featureTableExtras.map(({title}, i) => ({
+          ...(!featureTableExtras ? [] : featureTableExtras.map(({ title }, i) => ({
             text: title,
             align: "center"
           }))),
@@ -204,10 +207,10 @@ export default withStyles(theme => ({
             align: "center"
           }]}
           body={upTo20.map((e, i) => [
-            {text: i + 1, align: "center"},
-            ...(!featureTableExtras ? [] : featureTableExtras.map(({values}) => {
+            { text: i + 1, align: "center" },
+            ...(!featureTableExtras ? [] : featureTableExtras.map(({ values }) => {
               const Val = values[i]
-              const render = typeof Val === "function" ? <Val/> : Val
+              const render = typeof Val === "function" ? <Val /> : Val
               return { text: render, align: "center" }
             })),
             featureNames[i] ? featureNames[i].join(", ") : "---"
@@ -215,33 +218,36 @@ export default withStyles(theme => ({
         />
       </Paper>
       <Features Section={Section}>{features}</Features>
-      <div className={classes.heroStudiesTitleMargin}/>
+      <div className={classes.heroStudiesTitleMargin} />
       <Section id="heroes" variant="title" title={`${name} Hero Studies`}>
         <Typography paragraph>
           At Level 3, you choose to study one of the Heroes shown below. You gain the features shown in the description of your chosen Hero, and you
           can choose to learn the abilities shown in that Hero's description.
         </Typography>
       </Section>
-      {heroes.map(({name, epithet, quote, desc: Desc, abilities, features}, i) => {
+      {heroes.map(({ name, epithet, quote, desc: Desc, abilities, features }, i) => {
         const heroSlug = `hero.${slug(name)}`
         return (
           <React.Fragment key={"hero-" + name}>
+            <div className={i > 0 ? classes.heroNameMargin : undefined} />
             <Section id={heroSlug} variant="h1" title={name} subtitle={epithet} className={classes.heroHeading}>
-              <Typography align="center"><i>"{quote.text}"</i></Typography>
-              <Typography align="center" paragraph>― {quote.author}</Typography>
-              <Desc/>
+              {quote && <React.Fragment>
+                <Typography align="center"><i>"{quote.text}"</i></Typography>
+                <Typography align="center" paragraph>― {quote.author}</Typography>
+              </React.Fragment>}
+              <Desc />
             </Section>
-            { abilities &&
+            {abilities &&
               <Section id={`${heroSlug}.abilities`} variant="h2" title="Abilities">
 
               </Section>
             }
-            <Section id={`${heroSlug}.features`} variant="h2" title="Features"/>
+            <Section id={`${heroSlug}.features`} variant="h2" title="Features" />
             <Paper className={classes.tablePaper}>
               <Table
                 head={["Level", "Feature"]}
                 body={[
-                  ...features.map(({levels: [level], title}, i) => [level, title])
+                  ...features.map(({ levels: [level], title }, i) => [level, title])
                 ]}
               />
             </Paper>
