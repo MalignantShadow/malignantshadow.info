8430f3edab23f6b09b8b4ef5b7d366e3b864c0ae
Shad0w <malignant.shadowborne@gmail.com>
2019-02-21 22:13:05 -0800

    Custom link component to prevent rerendering, wraps Link (MUI)

---
+++ src/components/asc/AscLink.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import Link from '@material-ui/core/Link'
+
+// Custom link component that won't cause a re-render if the pathname is the same
+// This should only be used on pages that don't rely on hash changes (which should be every page)
+
+export default withRouter(({href, children, history, location, match, params, staticContext, ...other}) => {
+  const [path, hash] = href.split("#")
+  const [locationFromProp, query = ""] = path.split("?")
+
+  // Use the handler if the location and query is the same
+  // We don't care if the hash is the same, cause the user could have scrolled
+  const useHandler = (!locationFromProp || location.pathname === locationFromProp) && query === location.search
+
+  function handleClick(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    const el = document.querySelector(`#${hash}`)
+    if(!el) return
+
+    // Using window.history instead of history prop to prevent causing a rerender
+    // This won't show in react-router's history prop, but I don't care about that
+    window.history.pushState({}, "", `${location.pathname}${href}`)
+
+    el.scrollIntoView({behavior: "smooth", block: "start"}) // 'block' is ignored in polyfill
+  }
+
+  return <Link href={href} onClick={useHandler ? handleClick : undefined} {...other}>{children}</Link>
+
+})
