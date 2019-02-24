f08f511a72fe41bcf69d36d0ae5e40b6fba9c8da
Shad0w <malignant.shadowborne@gmail.com>
2019-02-23 18:58:18 -0800

    Fix resistance/vulnerability to allow no arguments

--- src/lib/asc/game/dictionary.js
+++ src/lib/asc/game/dictionary.js
@@ -1,9 +1,9 @@
-const t = (name, desc, other) => ({name, desc, ...other})
+const t = (name, desc, other) => ({ name, desc, ...other })
 
 const damage = (noun, adj, doubled) => (args, variant) => {
   const is = doubled ? "doubled" : "halved"
-  const dmg = args[0][0].toUpperCase() + args[0].substring(1)
-  if(variant === "adj")
+  const dmg = args && args.length ? args[0][0].toUpperCase() + args[0].substring(1) : ""
+  if (variant === "adj")
     return t(`${adj} to ${dmg} damage`, `${dmg} damage is ${is}`)
   return t(noun, `Damage is ${is}`)
 }
