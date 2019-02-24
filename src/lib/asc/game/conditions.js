3dd7c10abc36776a7abf56883b8eec3abbda6c5f
Shad0w <malignant.shadowborne@gmail.com>
2019-02-23 17:46:19 -0800

    Add blinded condition

--- src/lib/asc/game/conditions.js
+++ src/lib/asc/game/conditions.js
@@ -8,7 +8,7 @@ import {
 
 const c = (name, desc, other) => ({ name, desc, ...other })
 
-const List = ({children, ...other}) => (
+const List = ({ children, ...other }) => (
   <ul {...other}>
     {children.map((e, i) => (
       <li key={i}><Typography>{e}</Typography></li>
@@ -16,22 +16,30 @@ const List = ({children, ...other}) => (
   </ul>
 )
 
-export const prone = c("Prone", ({disableTerms}) => (
+export const blinded = c("Blinded", ({ disableTerms }) => (
+  <List>
+    <React.Fragment>The unit cannot see and automatically fails any aptitude or skill checks that require sight.</React.Fragment>
+    <React.Fragment>Attack rolls against the unit have <GameTerm term="advantage" disableHover={disableTerms} /></React.Fragment>
+    <React.Fragment>Attack rolls made by the unit have <GameTerm term="disadvantage" disableHover={disableTerms} /></React.Fragment>
+  </List>
+))
+
+export const prone = c("Prone", ({ disableTerms }) => (
   <List>
     <React.Fragment>
       The unit's only movement option is to crawl or stand up. Standing up costs half the unit's movement and ends this condition.
     </React.Fragment>
     <React.Fragment>
-      The unit has <GameTerm term="disadvantage" disableHover={disableTerms}/> on attack rolls.
+      The unit has <GameTerm term="disadvantage" disableHover={disableTerms} /> on attack rolls.
     </React.Fragment>
     <React.Fragment>
-      Attack rolls against the unit have <GameTerm term="disadvantage" disableHover/>. If the attacker is within 5 feet, the attack roll
-      has <GameTerm term="advantage" disableHover={disableTerms}/> instead.
+      Attack rolls against the unit have <GameTerm term="disadvantage" disableHover />. If the attacker is within 5 feet, the attack roll
+      has <GameTerm term="advantage" disableHover={disableTerms} /> instead.
     </React.Fragment>
   </List>
 ))
 
-export const invisible = c("Invisible",  ({disableTerms}) => (
+export const invisible = c("Invisible", ({ disableTerms }) => (
   <List>
     <React.Fragment>
       The unit is impossible to see without special equipment or senses.
@@ -40,15 +48,16 @@ export const invisible = c("Invisible",  ({disableTerms}) => (
       The unit is considered heavily obscured, and can be revealed if it makes noise or tracks.
     </React.Fragment>
     <React.Fragment>
-      Attack rolls against the unit have <GameTerm term="disadvantage" disableHover={disableTerms}/>.
+      Attack rolls against the unit have <GameTerm term="disadvantage" disableHover={disableTerms} />.
     </React.Fragment>
     <React.Fragment>
-      Attacks rolls made by the unit have <GameTerm term="advantage" disableHover={disableTerms}/>.
+      Attacks rolls made by the unit have <GameTerm term="advantage" disableHover={disableTerms} />.
     </React.Fragment>
   </List>
 ))
 
 export default [
+  blinded,
   invisible,
   prone
 ]
