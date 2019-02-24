539687aa23fe2dc251c539db17f68dc4cef1c6f7
Shad0w <malignant.shadowborne@gmail.com>
2019-02-23 19:08:41 -0800

    Remove scmoothscroll and hash-link, as they are unsused now
    Implemented a smooth scroll myself /shrug

--- src/index.asc.js
+++ src/index.asc.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames'
-import smoothscroll from 'smoothscroll-polyfill'
 //import * as serviceWorker from './serviceWorker';
 
 import { withRouter, Link, Switch } from 'react-router-dom'
@@ -23,8 +22,6 @@ import Asc from './components/asc/icons/Asc'
 import routes from "./lib/asc/routeInfo"
 import pages from "./lib/asc/pageInfo"
 import { useRoutes } from "./lib/routing"
-
-smoothscroll.polyfill()
 
 const makeColor = (color, values = [300, 500, 700], other) => ({
   light: color[values[0]],
