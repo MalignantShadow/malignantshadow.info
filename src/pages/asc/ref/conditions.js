fdf86eb57c16dd2c8b1607025cd9950e8f1fa7bb
Shad0w <malignant.shadowborne@gmail.com>
2019-02-25 22:09:05 -0800

    Explain conditions more

--- src/pages/asc/ref/conditions.js
+++ src/pages/asc/ref/conditions.js
@@ -3,7 +3,7 @@ import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
 
 import {
-  AscPage
+  AscPage, ConditionTerm
 } from '../../../components/asc'
 import { slug } from '../../../lib/routing'
 import { styledTocItem, styledSection } from '../../../components/asc/util'
@@ -14,7 +14,7 @@ const TocItem = styledTocItem(colorSelector)
 const Section = styledSection(colorSelector)
 
 const toc = <React.Fragment>
-  <TocItem title href="">Conditions</TocItem>
+  <TocItem title href="">Reference: Conditions</TocItem>
   <Divider />
   {conditions.map((c, i) => (
     <TocItem key={i} href={`#${slug(c.name)}`}>{c.name}</TocItem>
@@ -26,7 +26,13 @@ const page = (
     <Section id="#topContent" variant="title" title="Conditions" subtitle="Reference Page">
       <Typography paragraph>
         A <b>Condition</b> is an effect applied to a unit that modifies which actions they are allowed to take. In other games, this is typically
-        called an <b>ailment</b> or <b>status effect</b>.
+        called an <b>ailment</b> or <b>status effect</b>. A Condition ends when an effect listed in the Condition's description occurs or when
+        an effect described in the source of the effect occurs. [example]
+      </Typography>
+      <Typography paragraph>
+        A unit being subjected to a Condition they already have does not make that Condition worse unless otherwise stated
+        (i.e. <ConditionTerm term="exhaustion" />). If a unit is subjected to an effect that causes them to suffer a condition that they already
+        suffer from, the duration of the condition becomes the larger of the two effects. [example]
       </Typography>
     </Section>
     {conditions.map(({ name, desc: Desc }, i) => (
